ea8033d88fddeba3060bfbd912c20bfc9dcb7984
biggest-keyboard-fan <nyancapt@gmail.com>
2019-12-21 14:43:54 +0300

    new scoreboard test

--- src/main/java/palindrome/main/MainPalindrome.java
+++ src/main/java/palindrome/main/MainPalindrome.java
@@ -12,10 +12,11 @@ TODO:
  - Look for other ways of cheating
  - FileHandler debugging
  - Reformat classes
+ - Override equals() for IOData, GameData, Word classes
 */
 
 public class MainPalindrome{
-  public static Boolean writeFilesToConsole = true;
+  public static Boolean writeFilesToConsole = false;
   public static void main(String[] args) throws IOException, ClassNotFoundException {
 
     GameHandler gHandler = new GameHandler(); // - game mode
